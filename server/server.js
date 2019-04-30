a717b12d4842f3823316e3d451afc2c72fac2654
Victor Senna <vsfm02@gmail.com>
2019-04-30 20:04:44 -0300

    server basic confg

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("<h1>Servidor rodando com ExpressJS</h1>");
+    res.send("<h1>SERVER TEST</h1>");
 });
 
-http.createServer(app).listen(3000, () => console.log('Server local rodando na porta 3000'));
+http.createServer(app).listen(3000, () => console.log('SERVER TEST'));
